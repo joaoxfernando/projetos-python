8c725ca1582e6cf9911032cd4a725ca3385aca99
João Fernando <joaofernando1988@gmail.com>
2021-06-15 19:22:34 -0300

    Validador de CPF e README.md do Gerador de CPF

---
+++ gerador_cpf/README.md
@@ -0,0 +1,30 @@
+# Gerador de CPF
+
+## ETAPAS DO GERADOR
+- O programa gera um número aleatório de 9 dígitos e que não seja sequencial (111.111.111 ou 555.555.555).
+- Fará a validação dos digitos do CPF seguindo o algoritmo encontrado logo abaixo.
+- Exibe o número do CPF gerado já com os digitos validados.
+- Pergunta se deseja gerar um novo CPF. 
+
+## ALGORITMO
+```
+Exemplo - CPF 325-875-343-13 (cpf gerado pelo gerador)
+--------------------------------------
+ALGORITMO PARA VALIDAR O DIGITO DO CPF
+---------------------------------------
+3 * 10  = 30        #   3 * 11  = 33
+2 * 9   = 18        #   2 * 10  = 20
+5 * 8   = 40        #   5 * 9   = 45
+8 * 7   = 56        #   8 * 8   = 64
+7 * 6   = 42        #   7 * 7   = 49
+5 * 5   = 25        #   5 * 6   = 30
+3 * 4   = 12        #   3 * 5   = 15
+4 * 3   = 12        #   4 * 4   = 16
+3 * 2   = 6         #   3 * 3   = 9
+        = 241       #   1 * 2   = 2
+                    #           = 283
+Fórmula para cálculo de cada digito:
+digito_1 = 11 - (soma % 11)
+digito_1 = 11 - (10) = 1
+digito_2 = 11 - (289 % 11) = 11 - 8 = 3`
+````
---
+++ validador_cpf/valida_cpf.py
@@ -0,0 +1,75 @@
+"""
+Programa para Validar CPF
+
+Exemplo - CPF 370.169.908-93
+------------------------------
+3 * 10  = 30        #   3 * 11  = 33
+7 * 9   = 63        #   7 * 10  = 70
+0 * 8   = 0         #   0 * 9   = 0
+1 * 7   = 7         #   1 * 8   = 8
+6 * 6   = 36        #   6 * 7   = 42
+9 * 5   = 45        #   9 * 6   = 54
+9 * 4   = 36        #   9 * 5   = 45
+0 * 3   = 0         #   0 * 4   = 0
+8 * 2   = 16        #   8 * 3   = 24
+        = 233       #   9 * 2   = 18
+                    #           = 294
+"""
+while True:
+    try:
+        cpf = int(input('Digite o número do CPF que deseja consultar: '))
+        é_valido = True
+    except:
+        cpf = 0
+        é_valido = False
+
+    if é_valido == False:
+        print('É necessário digitar apenas valores númericos.')
+    else:
+
+        cpf_digitado = str(cpf)
+        cpf_validado = cpf_digitado[:-2]
+        reverso = 10
+        soma = 0
+        tamanho_cpf = len(cpf_digitado)
+        checando_se_numerico = 0
+        if tamanho_cpf < 11 and é_valido:
+            print(f'O número que você digitou tem {tamanho_cpf} digitos apenas.')
+            print('É necessário informar 11 dígitos.\n')
+        elif tamanho_cpf > 11 and é_valido:
+            print(f'Você digitou um CPF com {tamanho_cpf} digitos.')
+            print('É necessário informar 11 digitos.')
+        else:
+            for index in range(19):
+                if index > 8:
+                    index -= 9
+
+                soma += int(cpf_validado[index]) * reverso
+                reverso -= 1
+
+                if reverso < 2:
+                    reverso = 11
+                    digito = 11 - (soma % 11)
+                    if digito > 9:
+                        digito = 0
+                    soma = 0
+                    cpf_validado += str(digito)
+
+
+            # Evita sequências como 111.111.111, 555.555.555, etc.
+            sequencia = cpf_validado == str(cpf_validado[0]) * len(cpf_digitado)
+
+            # Não deixará que um CPF sequencial seja validado
+            if cpf_digitado == cpf_validado and not sequencia:
+                cpf_digitado = cpf_digitado[:9] + '-' + cpf_digitado[-2:]
+                cpf_digitado = cpf_digitado[0:3] + '.' + cpf_digitado[3:6] + '.' + cpf_digitado[6:9] + cpf_digitado[9:]
+                print(f'O CPF {cpf_digitado} é valido!')
+            else:
+                 print(f'O CPF {cpf_digitado} não é valido!')
+            continuar = input('Deseja validar outro CPF? S/N: ').lower()
+
+            if continuar in 'Nn':
+                print('\nObrigado por utilizar nosso programa. \nEspero que tenha curtido!')
+                break
+            elif continuar not in 'Ss' and 'Nn':
+                continuar = input('Deseja validar outro CPF? S/N: ').lower()
